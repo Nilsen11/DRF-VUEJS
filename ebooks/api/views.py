54d8fe9b57d9fa3050abe5b7d3a48f17f828a568
Nilsen11 <Vansid99@gmail.com>
2019-06-20 23:51:58 +0300

    Added model serializer, view for ebook and review

---
+++ ebooks/api/views.py
@@ -0,0 +1,43 @@
+from rest_framework import generics
+from rest_framework import mixins
+from rest_framework.generics import get_object_or_404
+from ebooks.models import Review, Ebook
+from ebooks.api.serializers import ReviewSerializer, EbookSerializer
+
+
+# class EbookListCreateAPIView(mixins.ListModelMixin,
+#                              mixins.CreateModelMixin,
+#                              generics.GenericAPIView):
+#     queryset = Ebook.objects.all()
+#     serializer_class = EbookSerializer
+#
+#     def get(self, request, *args, **kwargs):
+#         return self.list(self, request, *args, **kwargs)
+#
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
+
+
+class EbookListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Ebook.objects.all()
+    serializer_class = EbookSerializer
+
+
+class EbookDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Ebook.objects.all()
+    serializer_class = EbookSerializer
+
+
+class ReviewCreateAPIView(generics.CreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def perform_create(self, serializer):
+        ebook_pk = self.kwargs.get("ebook_pk")
+        ebook = get_object_or_404(Ebook, pk=ebook_pk)
+        serializer.save(ebook=ebook)
+
+
+class ReviewDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
